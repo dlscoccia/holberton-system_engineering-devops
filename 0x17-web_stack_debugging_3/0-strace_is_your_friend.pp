808b26e66d10b8a93cd1cf42407849921c9db423
dlscoccia <2054@holbertonschool.com>
2021-02-19 02:01:22 +0000

    manifest puppeeeet

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,7 @@
- # fix php in the WordPress file wp-settings.php
+# fix php in the WordPress file wp-settings.php
 
 exec { 'fix-wordpress':
   command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
   path    => '/usr/local/bin/:/bin/'
 }
+
